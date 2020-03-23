3c5b9b5e99db0ef9b8611bf265aa9eacb04ef612
kaizendevar <pmartinacosta@gmail.com>
2020-03-23 01:02:46 -0300

    notificacion a bot telegram

--- index.js
+++ index.js
@@ -1,93 +1,101 @@
 const cheerio = require('cheerio'),
     puppeteer = require('puppeteer'),
     schedule = require("node-schedule"),
+    http = require("http"),
     Telegraf = require("telegraf");
 
-const ACCESS_TOKEN_TELEGRAM = "";
-const CHAT_ID = "";
+require('dotenv').config();
+
+// Modificar .env.development por .env indicando los dos valores
+const ACCESS_TOKEN_TELEGRAM = process.env.ACCESS_TOKEN_TELEGRAM;
+const CHAT_ID = process.env.CHAT_ID;
 
 const url = 'https://extranet.osam.org.ar/Consulta/embedFarmacias';
 
 class Tracker {
     constructor(telegramBot) {
         this.telegramBot = telegramBot;
-
-        this.farmacias = {
-            nombre: 0,
-            direccion: 0,
-            telefono: 0
-        };
     }
 
-    initialize = async () => {
-        // const data = await this.setDataAndNotify(); ??
-        // console.log('data', data);
-        this.setDataAndNotify();
+    initialize = () => {
+        this.getData();
 
         schedule.scheduleJob("0 */1 * * *", () => {
-            this.setDataAndNotify();
+            this.getData();
         });
     };
 
-    getDataFarmacias = async farmacias => {
-        try {
-            puppeteer
-            .launch()
-            .then(browser => browser.newPage())
-            .then(page => {
+    getDataFarmacias = () => new Promise ((res, rej) => {
+        puppeteer
+        .launch()
+        .then(browser => browser.newPage())
+        .then(page => {
             return page.goto(url).then(function() {
                 return page.content();
             });
-            })
-            .then(html => {
-                const $ = cheerio.load(html);
-                const farmacias = [];
-                $('div[id="farmacias"] > a > div').each(function() {
-                    const nombre = $(this).find('h3').text().trim();
-                    const direccion = $(this).find('h4').first().text().trim();
-                    const telefono = $(this).find('h4 ~ h4').text().trim();
-                
-                    farmacias.push({
-                        nombre: nombre,
-                        direccion: direccion,
-                        telefono: telefono,
-                    });
-                });
+        })
+        .then(html => {
+            const $ = cheerio.load(html);
+            const farmacias = [];
+            $('div[id="farmacias"] > a > div').each(function() {
+                const nombre = $(this).find('h3').text().trim();
+                const direccion = $(this).find('h4').first().text().trim().toUpperCase();
+                const telefono = $(this).find('h4 ~ h4').text().trim();
             
-                // var jsonObject = JSON.stringify(farmacias);
-                // http.createServer(function (req, res) {
-                //     res.writeHead(200, {'Content-Type': 'text/plain'});
-                //     res.end(jsonObject);
-                // }).listen(80);
+                farmacias.push({
+                    nombre: nombre,
+                    direccion: direccion,
+                    telefono: telefono,
+                });
+            });
 
+            res(farmacias);
+        })
+        .catch((error) => { console.error(error); rej(error) });
+})
 
-                // Devuelve bien objeto JSON con todas las farmacias
-                console.log('1 ', farmacias);
-                return farmacias;
-            })
-            .catch(console.error);
+    getDate() {
+        var fecha = new Date(); // Fecha actual
+        var mes = fecha.getMonth()+1; // obteniendo mes
+        var dia = fecha.getDate(); // obteniendo dia
+        var ano = fecha.getFullYear(); // obteniendo año
+        if(dia<10)
+            dia='0'+dia; // agrega cero si el menor de 10
+        if(mes<10)
+            mes='0'+mes // agrega cero si el menor de 10
 
-        } catch (err) {
-            throw new Error(`Error al obtener farmacias ${farmacias}`);
-        }
-    };
+        return dia+"/"+mes+"/"+ano;
+    }
 
-    setDataAndNotify = async () => {
-        this.farmacias = await this.getDataFarmacias();
+    BuilderString(farmacias, fecha) {
+        let TelegramMessage = farmacias.reduce((acum,farmacia) => (
+            acum + `${farmacia.nombre}\n\n${farmacia.direccion}\n${farmacia.telefono}\n\n ----- \n\n`
+        ),`Farmacias de Turno HOY ${fecha}:\nHASTA LAS 23:59hs \n \n`);        
 
-        // ERROR: No definido debido a que aún no terminó de obtener la información    
-        console.log('Farmacias: ', this.farmacias);
+        return TelegramMessage;
+    }
 
-        // val StringBuilder TelegramMessage = new StringBuilder
-        // let TelegramMessage = new StringBuilder;
-        // TelegramMessage.append(`Farmacias de Turno:`)
-        // TelegramMessage.append(`${this.farmacias.nombre} + ' ' + ${this.farmacias.nombre} + ${this.farmacias.telefono}`)
+    sendNotification(message) {
+        this.telegramBot.telegram.sendMessage(
+            CHAT_ID,
+            `${message}`
+        );   
+    }
 
-        // this.telegramBot.telegram.sendMessage(
-        //     CHAT_ID,
-        //     `${TelegramMessage.toString}`
-        // );        
-    };
+    showPage(message) {
+        http.createServer(function (req, res) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(message);
+        }).listen(80);
+    }    
+
+    async getData() { 
+        const fecha = this.getDate();
+        const farmacias = await this.getDataFarmacias();
+        const textofarmacias = this.BuilderString(farmacias, fecha);
+        this.sendNotification(textofarmacias);
+        this.showPage(textofarmacias);
+    }
 }
 
 const telegramBot = new Telegraf(ACCESS_TOKEN_TELEGRAM);
